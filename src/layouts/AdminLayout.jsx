4cd37b240a71141b0e78cebbfcba40d946cb3e63
ColleFALL <collefall118@gmail.com>
2026-01-21 17:31:49 +0000

    ajout bouton modifier mis a jour2

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -42,7 +42,7 @@ export default function AdminLayout() {
         <div className="flex-1 flex flex-col min-h-0">
           <Topbar onMenuClick={() => setSidebarOpen(true)} />
 
-          <main className="flex-1 min-h-0 overflow-y-auto px-4 py-4">
+          <main className="flex-1 min-h-0 overflow-y-auto ">
             <Outlet />
           </main>
         </div>
