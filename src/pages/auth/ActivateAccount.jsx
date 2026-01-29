9a572ed2ba765b14945e624611b84ca6732f1824
ColleFALL <collefall118@gmail.com>
2026-01-29 13:46:59 +0000

    correction des fetchapi6

--- src/pages/auth/ActivateAccount.jsx
+++ src/pages/auth/ActivateAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { activateAccountApi } from "../../services/authApi";
 
